14757cb7fd9a8a77eada99e6c02befb5f7abf22b
armandnur10 <zxuanggg@gmail.com>
2022-04-05 10:58:03 +0700

    first-commit

--- app/src/main/java/com/armand/notesapp/ui/update/UpdateFragment.kt
+++ app/src/main/java/com/armand/notesapp/ui/update/UpdateFragment.kt
@@ -1,6 +1,7 @@
 package com.armand.notesapp.ui.update
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatImageButton
@@ -61,30 +62,32 @@ class UpdateFragment : Fragment() {
     }
 
     private fun updateNote() {
-        val title = binding.edtTitleUpdate.text.toString()
-        val desc = binding.edtDescriptionUpdate.text.toString()
-        val priority = binding.spinnerPrioritiesUpdate.selectedItem.toString()
-        val date = Calendar.getInstance().time
+        binding.apply {
+            val title = edtTitleUpdate.text.toString()
+            val description = edtDescriptionUpdate.text.toString()
+            val priority = spinnerPrioritiesUpdate.selectedItem.toString()
 
-        val formatedDate = SimpleDateFormat("dd MMM yyyy", Locale.getDefault()).format(date)
+            val calender = Calendar.getInstance().time
+            val date = SimpleDateFormat("dd MMM yyyy", Locale.getDefault()).format(calender)
 
-        findNavController().navigate(R.id.action_updateFragment_to_detailFragment)
-        Toast.makeText(context, "Note has been Update", Toast.LENGTH_SHORT).show()
-
-        if (title.isEmpty()){
-            binding.edtTitleUpdate.error = "Please fill field"
-        } else if(desc.isEmpty()){
-            Toast.makeText(context, "Your note is still empty", Toast.LENGTH_LONG).show()
-        } else {
-            UpdateViewModel.updateNote(
-                Notes(saveArgs.currentItem.id,
+            val note = Notes(
+                saveArgs.currentItem.id,
                 title,
                 parseToPriority(priority, context),
-                desc,
-                formatedDate
-            ))
-            Toast.makeText(context, "successfull add note", Toast.LENGTH_SHORT).show()
-            findNavController().navigate(R.id.action_addFragment_to_homeFragment)
+                description,
+                date
+            )
+            if (edtTitleUpdate.text.isEmpty() || edtDescriptionUpdate.text.isEmpty()){
+                Toast.makeText(context, "Please fill fields", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                UpdateViewModel.insertData(note)
+                val action = UpdateFragmentDirections.actionUpdateFragmentToDetailFragment(note)
+                findNavController().navigate(action)
+                Toast.makeText(context, "Succesfully Update Note", Toast.LENGTH_SHORT).show()
+                Log.i("UpdateFragment", "updateNote: $note")
+            }
+
         }
     }
 
